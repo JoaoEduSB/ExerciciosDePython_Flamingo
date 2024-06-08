8a1cfd1c6651792805e3bb30d5d5b895d5e88a2e
João Eduardo <joao.eduardo.souza@hotmail.com>
2024-06-08 18:01:22 -0300

    Initial commit.py

--- Tarefas_5/Tarefa05_Ex03_Conversao.py
+++ Tarefas_5/Tarefa05_Ex03_Conversao.py
@@ -6,45 +6,24 @@
 # Exercício de contagem de vogais de uma frase
 
 #3) Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius (baseado na fórmula abaixo):
+# Curso Básico de Python
+# Nome do desenvolvedor: João Eduardo Souza Barbosa
+# Versão 1.0
+# Exercício de lógica de programação 
+# com a linguagem de programação Python
+# Exercício de contagem de vogais de uma frase
 
-# print ("\nVamos ler as notas do aluno e dizer se ele foi aprovado ou não\n")
+#3) Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius (baseado na fórmula abaixo):
 
-# nota1 = float(input("Digite a sua primeira nota: "))
+#C= (F - 32)*5/9
 
-# while (nota1 < 0) | (nota1 > 10):
-#     nota1 = float(input("A nota digitada é invalida.\n"
-# "Digite novamente a sua primeira nota: "))
+#Observação: Para testar se a sua resposta está correta saiba que 100oC = 212F
 
+print("\nVamos converter uma temperatura de Fahrenheit para celsius\n")
 
-# nota2 = float(input("Digite a sua segunda nota: "))
-# while (nota2 < 0) | (nota2 > 10):
-#     nota2 = float(input("A nota digitada é invalida.\n"
-# "Digite novamente a sua segunda nota: "))
+fahrenheit = float(input("Digite a temperatura em fahrenheit: "))
 
-# media = ((nota1 + nota2) / 2)
+celsius = ((fahrenheit - 32 ) * 5) / 9
 
-# if (media >= 6):
-#     print("O aluno foi aprovado com média:", media)
-# else:
-#     print("O aluno foi reprovado com média:", media)
-    
-print("Vamos calcular a nota média do aluno")
+print("A conversão de", fahrenheit, "°F, resulta em", celsius, "°C.\n")
 
-nota1 = float(input("Digite a primeira nota: "))
-
-while ((nota1 < 0) | (nota1 > 10)):
-    nota1 = float(input("Nota digitada é incorrta. Digite novamente a primeira nota: "))
-    
-nota2 = float(input("Digite a segunda nota: "))
-
-while ((nota2 < 0) | (nota2 > 10)):
-    nota2 = float(input("Nota digitada é incorrta. Digite novamente a segunda nota: "))
-    
-nota3 = float(input("Digite a terceira nota: "))
-
-while ((nota3 < 0) | (nota3 > 10)):
-    nota3 = float(input("Nota digitada é incorreta. Digite novamente a terceira nota: "))
-
-mediaponderada = (((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / 10)
-
-print("A média do aluno é:", mediaponderada)
