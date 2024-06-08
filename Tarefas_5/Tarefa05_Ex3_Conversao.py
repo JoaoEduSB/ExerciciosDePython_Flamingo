0e64c1ea20c526d99ef7b7919ec707e3601bd953
João Eduardo <joao.eduardo.souza@hotmail.com>
2024-06-08 17:54:14 -0300

    Update tarefa 5 exercicio 3

--- Tarefas_5/Tarefa05_Ex3_Conversao.py
+++ Tarefas_5/Tarefa05_Ex3_Conversao.py
@@ -7,44 +7,14 @@
 
 #3) Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius (baseado na fórmula abaixo):
 
-# print ("\nVamos ler as notas do aluno e dizer se ele foi aprovado ou não\n")
+#C= (F - 32)*5/9
 
-# nota1 = float(input("Digite a sua primeira nota: "))
+#Observação: Para testar se a sua resposta está correta saiba que 100oC = 212F
 
-# while (nota1 < 0) | (nota1 > 10):
-#     nota1 = float(input("A nota digitada é invalida.\n"
-# "Digite novamente a sua primeira nota: "))
+print("\nVamos converter uma temperatura de Fahrenheit para celsius\n")
 
+fahrenheit = float(input("Digite a temperatura em fahrenheit: "))
 
-# nota2 = float(input("Digite a sua segunda nota: "))
-# while (nota2 < 0) | (nota2 > 10):
-#     nota2 = float(input("A nota digitada é invalida.\n"
-# "Digite novamente a sua segunda nota: "))
+celsius = ((fahrenheit - 32 ) * 5) / 9
 
-# media = ((nota1 + nota2) / 2)
-
-# if (media >= 6):
-#     print("O aluno foi aprovado com média:", media)
-# else:
-#     print("O aluno foi reprovado com média:", media)
-    
-print("Vamos calcular a nota média do aluno")
-
-nota1 = float(input("Digite a primeira nota: "))
-
-while ((nota1 < 0) | (nota1 > 10)):
-    nota1 = float(input("Nota digitada é incorrta. Digite novamente a primeira nota: "))
-    
-nota2 = float(input("Digite a segunda nota: "))
-
-while ((nota2 < 0) | (nota2 > 10)):
-    nota2 = float(input("Nota digitada é incorrta. Digite novamente a segunda nota: "))
-    
-nota3 = float(input("Digite a terceira nota: "))
-
-while ((nota3 < 0) | (nota3 > 10)):
-    nota3 = float(input("Nota digitada é incorreta. Digite novamente a terceira nota: "))
-
-mediaponderada = (((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / 10)
-
-print("A média do aluno é:", mediaponderada)
+print("A conversão de", fahrenheit, "°F, resulta em", celsius, "°C.\n")
---
+++ Tarefas_6/Tarefa06_Ex1_Diferenca.py
@@ -0,0 +1,18 @@
+# Curso Básico de Python
+# Nome do desenvolvedor: João Eduardo Souza Barbosa
+# Versão 1.0
+# Exercício de lógica de programação 
+# com a linguagem de programação Python
+#1) Ler dois valores numéricos inteiros e apresentar o resultado da diferença do maior pelo menor valor.
+
+print("\nVamos ler 2 valores e apresentar a diferença entre o menor para o maior\n")
+
+valorA = int(input("Digite o primeiro valor: "))
+valorB = int(input("Digite o segundo valor: "))
+
+if (valorA > valorB):
+    resultado = valorA - valorB
+else:
+    resultado = valorB - valorA
+
+print("O resultado da diferença dos dois é :", resultado)
